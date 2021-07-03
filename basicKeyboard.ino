b884481999d328476af4de21ec0bd524e43c0f43
riley <parish.riley@gmail.com>
2021-07-03 16:36:34 -0600

    Inverted the logic of the jumping pin so it registers when not connected rather than triggering when connected.

--- basicKeyboard.ino
+++ basicKeyboard.ino
@@ -10,19 +10,16 @@ const int ReleasedMinThreshold = 300; // this is the minimum reading when there 
 const byte PinCount = 4;
 
 // you can have as many keys as you have analog pins
-const byte InputPins[PinCount] = {A0, A1, A2, A3};
-// how to program arbitrary letters
-const char KeyCodes[PinCount] = {'w', 'a', 's','d'};
-// how to use special keys
-//const char KeyCodes[PinCount] = {KEY_UP_ARROW, KEY_DOWN_ARROW, KEY_LEFT_ARROW, KEY_RIGHT_ARROW};
+const byte InputPins[PinCount] = {A0, A1};
+const char KeyCodes[PinCount] = {KEY_UP_ARROW, KEY_DOWN_ARROW};
 
 
 // every character/key has these attributes
 struct TouchInput {
-  byte analogPin;
-  char keycode;
-  movingAvg filter = movingAvg(20);
-  boolean wasPressed = false;
+    byte analogPin;
+    char keycode;
+    movingAvg filter = movingAvg(20);
+    boolean wasPressed = false;
 };
 
 // this is an array of TouchInput structs called "Pins" that contains "PinCount" items in it
@@ -30,44 +27,51 @@ TouchInput Pins[PinCount];
 
 
 void setup(){
-  Serial.begin(115200);
-
-  // loop through each of the keys and initialize them in the order defined in InputPins[] KeyCodes[]
-  for (int i = 0; i < PinCount; i++){
-    Pins[i].analogPin = InputPins[i];
-    Pins[i].keycode = KeyCodes[i];
-    Pins[i].filter.begin();
-  }
+    Serial.begin(115200);
+    // loop through each of the keys and initialize them in the order defined in InputPins[] KeyCodes[]
+    for (int i = 0; i < PinCount; i++){
+        Pins[i].analogPin = InputPins[i];
+        Pins[i].keycode = KeyCodes[i];
+        Pins[i].filter.begin();
+    }
 }
 
 
-void loop()
-{
-  // evaluate each of the pins defined earlier
-  for (int i = 0; i < PinCount; i++){
-    
-    float currentAverage = Pins[i].filter.reading(analogRead(Pins[i].analogPin));
-    boolean previousState = Pins[i].wasPressed;
-    boolean currentState = previousState; // Default if in the dead zone
+void loop(){
+    // evaluate each of the pins defined earlier
+    for(int i = 0; i < PinCount; i++){
+        
+        float currentAverage = Pins[i].filter.reading(analogRead(Pins[i].analogPin));
+        boolean previousState = Pins[i].wasPressed;
+        boolean currentState = previousState; // Default if in the dead zone
 
 
-    if (currentAverage < PressedMaxThreshold){
-      currentState = true;      // this means that the circuit has been completed, so the key is currently being pressed
-    }
-    else if  (currentAverage > ReleasedMinThreshold){
-      currentState = false;      // the circuit has been broken, release
-    }
+        if(currentAverage < PressedMaxThreshold){
+            currentState = true;            // this means that the circuit has been completed, so the key is currently being pressed
+        }else if(currentAverage > ReleasedMinThreshold){
+            currentState = false;            // the circuit has been broken, release
+        }
 
-    if (currentState != previousState){
-      // "type" the characters
-      if (currentState){
-        Keyboard.press(Pins[i].keycode);
-      }else{
-        Keyboard.release(Pins[i].keycode);
-      }
-          
+        // if the state of the pin is different from the previous iteration
+        if(currentState != previousState){
+            // invert the state of the key press
+            if(currentState){
+                if(Pins[i].analogPin == A0){
+                    // the logic for this pin (jumping) is inverted (activate when there is no connection)
+                    Keyboard.release(Pins[i].keycode);
+                }else{
+                    // if it's not the jumping pin (in this case, crouching)
+                    Keyboard.press(Pins[i].keycode);
+                }
+            }else{
+                if(Pins[i].analogPin == A0){
+                    Keyboard.press(Pins[i].keycode);
+                }else{
+                    Keyboard.release(Pins[i].keycode);
+                }
+            }
+        }
+        Pins[i].wasPressed = currentState;
     }
-    Pins[i].wasPressed = currentState;
-  }
 }
 
