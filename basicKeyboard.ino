093f4225006c24422c54de127538599a8ad0c74b
riley <parish.riley@gmail.com>
2021-07-03 16:45:30 -0600

    Adjusted formatting and some comments.

--- basicKeyboard.ino
+++ basicKeyboard.ino
@@ -19,10 +19,10 @@ const char KeyCodes[PinCount] = {'w', 'a', 's','d'};
 
 // every character/key has these attributes
 struct TouchInput {
-  byte analogPin;
-  char keycode;
-  movingAvg filter = movingAvg(20);
-  boolean wasPressed = false;
+    byte analogPin;
+    char keycode;
+    movingAvg filter = movingAvg(20);
+    boolean wasPressed = false;
 };
 
 // this is an array of TouchInput structs called "Pins" that contains "PinCount" items in it
@@ -30,44 +30,42 @@ TouchInput Pins[PinCount];
 
 
 void setup(){
-  Serial.begin(115200);
+    Serial.begin(115200);
 
-  // loop through each of the keys and initialize them in the order defined in InputPins[] KeyCodes[]
-  for (int i = 0; i < PinCount; i++){
-    Pins[i].analogPin = InputPins[i];
-    Pins[i].keycode = KeyCodes[i];
-    Pins[i].filter.begin();
-  }
+    // loop through each of the keys and initialize them in the order defined in InputPins[] KeyCodes[]
+    for(int i = 0; i < PinCount; i++){
+        Pins[i].analogPin = InputPins[i];
+        Pins[i].keycode = KeyCodes[i];
+        Pins[i].filter.begin();
+    }
 }
 
 
-void loop()
-{
-  // evaluate each of the pins defined earlier
-  for (int i = 0; i < PinCount; i++){
-    
-    float currentAverage = Pins[i].filter.reading(analogRead(Pins[i].analogPin));
-    boolean previousState = Pins[i].wasPressed;
-    boolean currentState = previousState; // Default if in the dead zone
+void loop(){
+    // evaluate each of the pins defined earlier
+    for(int i = 0; i < PinCount; i++){
 
+        float currentAverage = Pins[i].filter.reading(analogRead(Pins[i].analogPin));
+        boolean previousState = Pins[i].wasPressed;
+        boolean currentState = previousState;   // Default if in the dead zone
 
-    if (currentAverage < PressedMaxThreshold){
-      currentState = true;      // this means that the circuit has been completed, so the key is currently being pressed
-    }
-    else if  (currentAverage > ReleasedMinThreshold){
-      currentState = false;      // the circuit has been broken, release
-    }
+        if(currentAverage < PressedMaxThreshold){
+            currentState = true;        // this means that the circuit has been completed, so the key should be pressed
+        }
+        else if(currentAverage > ReleasedMinThreshold){
+            currentState = false;       // the circuit has been broken, so the key should be released
+        }
 
-    if (currentState != previousState){
-      // "type" the characters
-      if (currentState){
-        Keyboard.press(Pins[i].keycode);
-      }else{
-        Keyboard.release(Pins[i].keycode);
-      }
-          
+        // if the state of the pin has changed since the last iteration
+        if(currentState != previousState){
+            // then start or stop pressing the key (the opposite of what it was before)
+            if(currentState){
+                Keyboard.press(Pins[i].keycode);
+            }else{
+                Keyboard.release(Pins[i].keycode);
+            }         
+        }
+        Pins[i].wasPressed = currentState;
     }
-    Pins[i].wasPressed = currentState;
-  }
 }
 
