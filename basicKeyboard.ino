6eacbb15bb3b43bea5dd7db0757e9e512e01187b
Riley Parish <parish.riley@gmail.com>
2021-10-27 20:11:40 -0600

    Update basicKeyboard.ino

--- basicKeyboard.ino
+++ basicKeyboard.ino
@@ -1,3 +1,12 @@
+/*
+Hey there! This code was originally written for our YouTube channel, https://www.youtube.com/RKadeGaming
+We're glad that you've taken an interest in our project, and we hope that you have a good time building it!
+We've made this code public, and you're free to use it however you like. If you end up sharing the project with others though, 
+we'd appreciate some attribution so that people know where to find more stuff like this.
+Thanks, and have fun! :)
+*/
+
+
 #include <Keyboard.h>
 #include <movingAvg.h>
 // “Sketch” menu > Include Library > Manage Libraries… > Search for “movingavg” > Install
